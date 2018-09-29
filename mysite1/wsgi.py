fd19b431439bd46d35c4fcb86f1158c28db92d9f
Zoya Khan <zmkhan@unomaha.edu>
2018-09-29 13:16:27 -0500

    initial commit

--- mysite1/wsgi.py
+++ mysite1/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'basicblog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite1.2.settings')
 
 application = get_wsgi_application()
